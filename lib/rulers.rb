21a3374764df16e11f0683b845952c4084db555d
Jon Schneider <jon.schneider@gmail.com>
2020-05-15 11:42:17 -0400

    Exercise the new sum method in the response

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -4,8 +4,17 @@ require 'rulers/array'
 module Rulers
   class Application
     def call(env)
-      [200, { 'Content-Type' => 'text/html' },
-       ['Hello from Jon\'s implementation of Ruby on Rulers!']]
+      # Get the comma-separated numeric values of the "args" query string key, if there was one.
+      # e.g. "http://localhost:3001/?args=1,2,3" will yield [1,2,3]
+      args = Rack::Utils.parse_nested_query(env['QUERY_STRING'])['args']&.split(',')&.map(&:to_i)
+
+      message = "<p>Hello from Jon's implementation of Ruby on Rulers!</p>"
+
+      unless args.nil?
+        message += "<p>Sum of the given arguments: #{args.sum}</p>"
+      end
+
+      [200, { 'Content-Type' => 'text/html' }, [message]]
     end
   end
 end
