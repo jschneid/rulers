402266f9f9c0d77f4389f7f094050e1baf43a756
Jon Schneider <jon.schneider@gmail.com>
2020-05-15 12:17:02 -0400

    Add more methods to the Array class

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -11,7 +11,9 @@ module Rulers
       message = "<p>Hello from Jon's implementation of Ruby on Rulers!</p>"
 
       unless args.nil?
+        message += "<p>The provided arguments were: #{args.pretty_print}</p>"
         message += "<p>Sum of the given arguments: #{args.sum}</p>"
+        message += "<p>Product of the given arguments: #{args.multiply}</p>"
       end
 
       [200, { 'Content-Type' => 'text/html' }, [message]]
--- lib/rulers/array.rb
+++ lib/rulers/array.rb
@@ -2,4 +2,12 @@ class Array
   def sum(start = 0)
     inject(start, &:+)
   end
+
+  def multiply
+    inject(:*)
+  end
+
+  def pretty_print(start = 0)
+    join(', ')
+  end
 end
